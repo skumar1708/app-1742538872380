690669b036f356b0c058084d1f6fb97737d40c75
Sharvan Sharma <itbhu.sk@gmail.com>
2025-03-21 12:07:32 +0530

    Automated commit: frontend/src/components/SplashScreen.js

---
+++ frontend/src/components/SplashScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function SplashScreen() {
+  return (
+    <div className="splash-screen">
+      <h1>Loading...</h1>
+    </div>
+  );
+}
+
+export default SplashScreen;
