c7885f75f790985ec01f169c319dc07386c7a5a7
Sharvan Sharma <itbhu.sk@gmail.com>
2025-03-21 12:07:08 +0530

    Automated commit: frontend/src/App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './components/Header';
+import Home from './pages/Home';
+import Footer from './components/Footer';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app-container">
+      <Header />
+      <Home />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
